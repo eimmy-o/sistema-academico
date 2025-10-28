6000d1665e6c1248e9c3d92d240579bc7b7b1572
elwese <cesar.ews20@gmail.com>
2025-10-27 23:34:07 -0500

    Componentes usuario estudiante

---
+++ frontend/src/pages/Account/EstudianteInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const EstudianteInfo = () => {
+  return (
+    
+   
+      <div class="tabla-info">
+        <table> 
+          <tr>
+            <th>
+              Matricula:
+            </th>
+            <td>
+              20136546
+            </td>
+
+          </tr>
+
+        </table>
+  <div><strong>Matrícula:</strong> 2022103456</div>
+  <div><strong>Carrera:</strong> Ingeniería en Sistemas</div>
+  <div><strong>Fecha de ingreso:</strong> 15/03/2022</div>
+  <div><strong>Materias en curso:</strong> 5</div>
+  <div><strong>Materias aprobadas:</strong> 20</div>
+  <div><strong>Materias reprobadas:</strong> 2</div>
+</div>
+
+
+  
+  )
+}
--- frontend/src/pages/Account/MyAccount.jsx
+++ frontend/src/pages/Account/MyAccount.jsx
@@ -1,6 +1,65 @@
+import { EstudianteInfo } from "./EstudianteInfo"
 
 export const MyAccount = () => {
   return (
-    <div>MyAccount</div>
+    <div className="account-page-container">
+      <h1>banneeeeeer</h1>
+
+      <div className="account-page-contents">
+
+        
+        <div class="tabla-info">
+  <div class="perfil-info">
+    <table class="tabla-perfil">
+      <tr>
+        <th>Apellidos:</th>
+        <td>Delgado Cañarte</td>
+      </tr>
+      <tr>
+        <th>Nombre:</th>
+        <td>Cesar Johan</td>
+      </tr>
+      <tr>
+        <th>Telefono:</th>
+        <td>09888989416</td>
+      </tr>
+      <tr>
+        <th>Genero:</th>
+        <td>Masculino</td>
+      </tr>
+      <tr>
+        <th>Direccion:</th>
+        <td>Mucho lote</td>
+      </tr>
+      <tr>
+        <th>Fecha de nacimiento:</th>
+        <td>18/08/2000</td>
+      </tr>
+      <tr>
+        <th>Cedula:</th>
+        <td>130569994413</td>
+      </tr>
+      <tr>
+        <th>Ciudad:</th>
+        <td>Guayaquil</td>
+      </tr>
+      <tr>
+        <th>Provincia:</th>
+        <td>Guayas</td>
+      </tr>
+    </table>
+  </div>
+
+  <div class="perfil-foto">
+    <img src="tu_imagen.jpg" alt="Foto de perfil" />
+  </div>
+</div>
+
+
+        <EstudianteInfo />
+      </div>
+
+
+    </div>
   )
 }
