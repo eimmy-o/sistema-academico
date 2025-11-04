2bf25d4db96c7eda1c6670fae3704f0d9e4df228
eimmy-o <eimvocho@espol.edu.ec>
2025-11-04 14:58:29 -0500

    Migracion de DashBoard to Courses + Link a las clases individuales

--- frontend/src/pages/Courses/CoursesRoutes.jsx
+++ frontend/src/pages/Courses/CoursesRoutes.jsx
@@ -8,30 +8,12 @@ import { CoursesNavBar } from "./CoursesNavBar"
 import { Box, IconButton, useMediaQuery } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu'
 import { useState } from "react"
+import { Course } from "./Course"
 
 export const CoursesRoutes = () => {
-    const isMobile = useMediaQuery('(max-width: 768px)')
-    const [isMenuClicked, setIsMenuClicked] = useState(false)
-
     return (
-        <Box sx={{padding: {xs: 0, md: '2rem 1.5rem'}, width: '100%'}}>
-            {isMobile ? 
-                <IconButton onClick={() => setIsMenuClicked(!isMenuClicked)}>
-                    <MenuIcon/> 
-                </IconButton> : 
-                <>
-                    <CoursesNavBar />
-                    <hr style={{marginInline: '.5rem'}}/>
-                </>
-            }
-            {isMenuClicked && isMobile && <CoursesNavBar flexDirection="column" isMobile={isMobile}/>}
             <Routes>
-                <Route path="/" element={<CoursesHome />}/>
-                <Route path="/grades" element={<Grades />}/>
-                <Route path="/attendance" element={<AttendancePage />}/>
-                <Route path="/assessments" element={<AssessmentPage />}/>
-                <Route path="/users" element={<Users />}/>
+                <Route path="/" element={<Course />}/>
             </Routes>
-        </Box>
-    )
+        )
 }
