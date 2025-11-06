d4d7499907de2568fcf9cf81ef39e91fd06c5d6b
elwese <cesar.ews20@gmail.com>
2025-11-06 08:35:31 -0500

    Add administrator profile and header components
    Introduces AdministradorProfile, AdministradorHeader, and related CSS for displaying and editing administrator details. Updates AdminNavBar to use the new profile component and integrates it into AdministradorPage, providing a structured and editable administrator profile view.

--- frontend/src/pages/Account/administrador/AdministradorPage.jsx
+++ frontend/src/pages/Account/administrador/AdministradorPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useEffect, useState } from "react";
 import ThemeToggle from "../estudiante/components/ThemeToggle";
+import { AdminNavBar } from './components/AdminNavBar';
 export const AdministradorPage = () => {
     const [administradores, setAdministradores] = useState([]);
   return (
       
       <div className="page-container">
           <ThemeToggle></ThemeToggle>
-          
+          <AdminNavBar></AdminNavBar>
   
       </div>
     );
--- frontend/src/pages/Account/administrador/components/AdminNavBar.jsx
+++ frontend/src/pages/Account/administrador/components/AdminNavBar.jsx
@@ -1,9 +1,53 @@
 import { useState } from "react";
-import { ProfessorTabs } from "./ProfessorTabs";
-import ProfessorProfile from "./ProfessorProfile";
+import AdministradorProfile from "./AdministradorProfile";
+
+// 1) estado en el padre
+const initialAdmin = {
+  firstName: "Alejandra",
+  lastName: "Gomez",
+  code: "PCR-354978",
+  status: { label: "Activo", tone: "success" },
+  scholarshipText: "Profesor",
+  lastAccess: "12 Oct 2025 · 10:42",  
+  photoUrl: "../../src/assets/perfilMujer.png",           // mejor desde /public
+  email: "alejan-gomez@adem.edu",
+  phone: "+593 998746234",
+  address: "Isla trinitaria",
+  birthDate: "1999-12-29",
+  gender: "F",
+  documentType: "Cédula",
+  idNumber: "1315648",
+  maritalStatus: "Casada",
+  enrollment: "PCR-146879",
+  enrollmentDate: "2018-03-14",
+  province: "Guayas",
+  city: "Guayaquil",
+};
 
 export const AdminNavBar = () => {
+  const [administrador, setAdmin] = useState(initialAdmin);  
+
+
+
+  // 2) guardar: actualiza el estado con lo editado
+  const handleSave = (patch) => {
+    setAdmin((prev) => ({ ...prev, ...patch }));
+    
+    // codigo que usare despues cuando me conecta con la api
+    // await api.updateStudent(prev.id, patch);
+  };
+  
   return (
-    <div>AdminNavBar</div>
-  )
-}
+    <div>
+
+        <AdministradorProfile
+          administrador={administrador}
+          onEdit={() => {}}
+          onToggleStatus={() => {}}
+          onSave={handleSave}
+        />
+
+
+    </div>
+  );
+};
